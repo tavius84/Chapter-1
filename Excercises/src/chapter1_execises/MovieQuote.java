1331c8d84f6bc61cf75f9e45112c6c29185d830d
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-27 09:35:19 -0500

    Movie

---
+++ Excercises/src/chapter1_execises/MovieQuote.java
@@ -0,0 +1,10 @@
+package chapter1_execises;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Excercises/src/chapter1_execises/TableAndChairs.java
+++ Excercises/src/chapter1_execises/TableAndChairs.java
@@ -5,7 +5,33 @@ public class TableAndChairs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Hello World");
+        
          
+        
+         System.out.println("x                   x");
+         System.out.println("x                   x");
+         System.out.println("x                   x");
+         System.out.println("x                   x");
+         System.out.println("x     xxxxxxxxx     x");
+         System.out.println("xxxxx x       x xxxxx");
+         System.out.println("x   x           x   x");
+      
+         displayTruck();     
 	}
+
+
+	  public static void displayTruck() {
+		
+		
+    System.out.println("----------------------");
+	System.out.println("|                    [|");
+	System.out.println("|                    [|");
+	System.out.println("|                     ------|");
+	System.out.println("|                           |");
+	System.out.println("|---------------------------|");
+	System.out.println("O                           O");
+	
+}
+
 
 }
