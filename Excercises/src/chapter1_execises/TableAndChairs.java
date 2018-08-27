1ab57f6f10bac8ecdbdea08abdba5d5e78cc3fdf
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-27 09:07:08 -0500

    Updated Text

---
+++ Excercises/src/chapter1_execises/TableAndChairs.java
@@ -0,0 +1,11 @@
+package chapter1_execises;
+
+public class TableAndChairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World");
+         
+	}
+
+}
