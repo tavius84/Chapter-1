30647fb0c1d8353f8301c9111e926d81a019dcb8
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-28 09:36:18 -0500

    updated

--- Excercises/src/chapter1_execises/RandomGuess.java
+++ Excercises/src/chapter1_execises/RandomGuess.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 public class RandomGuess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
        
 		JOptionPane.showMessageDialog(null, "The Number is "+(1 + (int)(Math.random() *  10)));
 		
