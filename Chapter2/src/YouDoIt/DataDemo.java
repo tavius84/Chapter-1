1352a01d8c754902ab71a757b760f6181c97f3c5
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-29 09:40:54 -0500

    updated 2

--- Chapter2/src/YouDoIt/DataDemo.java
+++ Chapter2/src/YouDoIt/DataDemo.java
@@ -4,7 +4,11 @@ public class DataDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        final int STATES_IN_US = 50;
+		int aWholeNumber = 315; 
+		System.out.print("the number is ");
+		System.out.println(aWholeNumber);
+		System.out.println("The number of states is " + STATES_IN_US);
 	}
 
 }
---
+++ Chapter2/src/YouDoIt/IntegerDemo.java
@@ -0,0 +1,19 @@
+package YouDoIt;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double anInt = 1234;
+        byte aByte = 12;
+        short aShort = 12;
+        long aLong = 1234567890987654321L;
+        double anotherInt = anInt * 10000000; 
+        System.out.println("The int is " + anInt );
+        System.out.println("The byte is " + aByte);
+        System.out.println("The short is " + aShort);
+        System.out.println("The long is " + aLong);
+        System.out.println("Another int is " + anotherInt);
+	}
+
+}
