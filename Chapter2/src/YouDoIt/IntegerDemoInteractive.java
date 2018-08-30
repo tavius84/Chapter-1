8f8d9dcd465724a1cc466f979bd3351d61e241ce
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-30 09:49:33 -0500

    Updated 2

--- Chapter2/src/YouDoIt/IntegerDemoInteractive.java
+++ Chapter2/src/YouDoIt/IntegerDemoInteractive.java
@@ -1,19 +1,38 @@
 package YouDoIt;
 
+import java.util.Scanner;
+
 public class IntegerDemoInteractive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        double anInt = 1234;
-        byte aByte = 12;
-        short aShort = 12;
-        long aLong = 1234567890987654321L;
-        double anotherInt = anInt * 10000000; 
+        double anInt;
+        byte aByte; 
+        short aShort; 
+        long aLong; 
+        String name; 
+        
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Please enter an integer");
+        anInt = input.nextInt();
+        System.out.println("Please enter an Byte");
+        aByte = input.nextByte();
+        System.out.println("Please enter an Short");
+        aShort = input.nextShort();
+        System.out.println("Please enter an Long");
+        aLong = input.nextLong();
+     
+        System.out.println("What is your name >> "); 
+        name = input.nextLine(); 
+        
+        
+        System.out.println("your name is " + name); 
         System.out.println("The int is " + anInt);
         System.out.println("The byte is " + aByte);
         System.out.println("The short is " + aShort);
         System.out.println("The long is " + aLong);
-        System.out.println("Another int is " + anotherInt);
+        
 	}
 
 }
