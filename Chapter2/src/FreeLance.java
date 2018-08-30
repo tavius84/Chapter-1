c335790ff87fd520fbf41acd8d0f846dbe8222e5
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-30 09:54:15 -0500

    Update 2

---
+++ Chapter2/src/FreeLance.java
@@ -0,0 +1,16 @@
+
+public class FreeLance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int num1;
+       int num2;
+       String name;
+       int total; 
+       
+       Scanner 
+       
+       
+	}
+
+}
