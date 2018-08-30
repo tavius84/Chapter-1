bc2cd6e0f94a4864b0eaa73092bd1b41ad273155
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-08-30 09:21:57 -0500

    Update 2

--- Chapter2/src/YouDoIt/IntegerDemoInteractive.java
+++ Chapter2/src/YouDoIt/IntegerDemoInteractive.java
@@ -1,6 +1,6 @@
 package YouDoIt;
 
-public class IntegerDemo {
+public class IntegerDemoInteractive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,7 +9,7 @@ public class IntegerDemo {
         short aShort = 12;
         long aLong = 1234567890987654321L;
         double anotherInt = anInt * 10000000; 
-        System.out.println("The int is " + anInt );
+        System.out.println("The int is " + anInt);
         System.out.println("The byte is " + aByte);
         System.out.println("The short is " + aShort);
         System.out.println("The long is " + aLong);
