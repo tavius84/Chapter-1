0415e77cea105d04b9be0224271587ad258642b6
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-09-04 09:51:20 -0500

    Update 2

---
+++ Chapter2/src/YouDoIt/QuartsToGallons.java
@@ -0,0 +1,27 @@
+package YouDoIt;
+
+public class QuartsToGallons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		int QUARTS_IN_GALLONS = 4 ; 
+	    Quarts remaining; 
+		total Gallons;
+		
+		
+		System.out.println("");
+		System.out.println(""); 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
