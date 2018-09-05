e8d2b04e57f1f362f87997eafd99e2c904cd01b6
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-09-05 09:49:27 -0500

    update 2

---
+++ Chapter2/src/YouDoIt/NauticalMiles.java
@@ -0,0 +1,24 @@
+package YouDoIt;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		double Kilometers = 1.852;
+		double Miles = 1.50779;
+		double nauticalMiles; 
+		double nauticalMilesToMiles;
+		double nauticalMilesToKilometers;
+		double nauticalMilesEntered = 20; 
+		
+		nauticalMiles = nauticalMilesEntered * Miles; 
+		
+		
+		
+		
+		
+		
+	}
+
+}
