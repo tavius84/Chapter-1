d1c3ad49f04f75bd1e8b7a91f902e0553ed2d114
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-09-10 09:13:51 -0500

    update 2

--- Chapter2/src/YouDoIt/InchesToFeet.java
+++ Chapter2/src/YouDoIt/InchesToFeet.java
@@ -5,6 +5,10 @@ public class InchesToFeet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         
+		int feetRemaining;
+		int inchesRemaining;
+		int feet = 12; 
+		int inchesEntered = 86;
 		
 		
 		
