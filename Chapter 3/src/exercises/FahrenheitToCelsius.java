0c2163f4ae415ae768b70aa2dc3f00ce328a4ee5
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-09-12 08:32:47 -0500

    update 3

--- Chapter 3/src/exercises/FahrenheitToCelsius.java
+++ Chapter 3/src/exercises/FahrenheitToCelsius.java
@@ -1,12 +1,31 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class FahrenheitToCelsius {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        
+         Scanner input = new Scanner(System.in); 
+    	 double ftemp; 
+         System.out.println("what is the temp at 8? "); 
+         ftemp = input.nextDouble();  
+	
 		 
 		
+		
+		displayInfo(ftemp); 
+		
+		
+	}
+
+	private static void displayInfo(double ftemp) {
+		// TODO Auto-generated method stub
+		
+		double Celsius;
+	
+		Celsius = ; 
+		System.out.println("The temp in Celsius at 8:00am is " + Celsius); 
 		
 		
 		
