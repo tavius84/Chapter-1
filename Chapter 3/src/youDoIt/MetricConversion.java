827d137dfe35e5a6bd05b53d07879f11d6bcb6af
tavius84 <42647558+tavius84@users.noreply.github.com>
2018-09-17 08:57:54 -0500

    update debugging 2

--- Chapter 3/src/youDoIt/MetricConversion.java
+++ Chapter 3/src/youDoIt/MetricConversion.java
@@ -2,7 +2,7 @@ package youDoIt;
 
 import java.util.Scanner;
 
-public class MetricCinversion {
+public class MetricConversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ Chapter2/src/debugging/FixDebugTwo.java
@@ -0,0 +1,15 @@
+package debugging;
+
+public class FixDebugTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+	      a = 7;
+	      b = 4;
+	      System.out.println("The sum is " + (a + b));
+	      System.out.println("The difference is " + (a - b));
+	      System.out.println("The product is " + (a * b));
+	}
+
+}
